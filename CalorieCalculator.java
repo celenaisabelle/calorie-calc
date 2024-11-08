ff4a9cfb9394c94a59d6e71effd9db14a0b35ff5
Celena Valenzuela <celenaisabelle@outlook.com>
2024-11-07 19:45:29 -0500

    First commit.

--- CalorieCalculator.java
+++ CalorieCalculator.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+//globally declared input method
+static Scanner input = new Scanner(System.in);
 //Calories Calclulator class
 public class CalorieCalculator {
 
+    //ask for userinput for the two daily questions
+    public static void getUserInput() {
+        
+    }
+
     //function main
     public static void main(String[] args) {
+        getUserInput();
 
+        calculateCalories();
+
+        displayCalories();
     }
 }
