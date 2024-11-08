7c3fbc2a7af4c8819fb430549e7bb7209f89643c
Celena Valenzuela <celenaisabelle@outlook.com>
2024-11-07 19:54:52 -0500

    Psuedo code progress.

--- CalorieCalculator.java
+++ CalorieCalculator.java
@@ -1,6 +1,28 @@
 import java.util.Scanner;
 //globally declared input method
 static Scanner input = new Scanner(System.in);
+
+//globally declared integers
+static int day1CalConsumed, day1CalBurned, day2CalConsumed, day2CalBurned,
+day3CalConsumed, day3CalBurned, day4CalConsumed, day4CalBurned,
+day5CalConsumed, day5CalBurned, day6CalConsumed, day6CalBurned,
+day7CalConsumed, day7CalBurned;
+
+//output daily questions and ask for user input
+public static void getUserInput() {
+    //get user input
+}
+
+//method to calculate total amount of calories burned for the week
+public static double calculateCalories() {
+    //formula to calculate calories
+}
+
+//display calories calculated for the week to output
+public static void displayCalories() {
+    //print calculated calories to output
+}
+
 //Calories Calclulator class
 public class CalorieCalculator {
 
@@ -11,6 +33,9 @@ public class CalorieCalculator {
 
     //function main
     public static void main(String[] args) {
+        int count;
+        double calories;
+
         getUserInput();
 
         calculateCalories();
